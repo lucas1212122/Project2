f485e1d5244576e245294d62407f4712f635120f
GabrielNuness <46833184+GabrielNuness@users.noreply.github.com>
2019-07-20 21:35:29 -0300

    Inimigo
    Inimigo feito
    Inimigo atacando
    Falta polir a movimentação

--- Project2/Assets/Scripts/Inimigo.cs
+++ Project2/Assets/Scripts/Inimigo.cs
@@ -5,16 +5,105 @@ using UnityEngine;
 public class Inimigo : MonoBehaviour
 {
     public int _vida;
+    public int _dano;
+    Player _playerScript;
+    public GameObject _areaDeDano, _player;
+    Animator _anim;
+    bool podeAtacar = true, podeAndar = true;
+
     void Start()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _anim = GetComponent<Animator>();
+        _playerScript = _player.GetComponent<Player>();
     }
 
     void Update()
     {
-        if(_vida <= 0)
+        if (_vida <= 0)
         {
-            Destroy(gameObject);
+            Morrer();
+        }
+        seguir();
+        ataque();
+    }
+
+    //Ataque//
+    void ataque()
+    {
+        if (Vector2.Distance(transform.position, _player.transform.position) <= 2 && podeAtacar)
+        {
+            _anim.SetTrigger("ataque");
+            _anim.SetBool("andando", false);
+            podeAtacar = false;
+            podeAndar = false;
+            darDano();
+            StartCoroutine(tempodeAtaque());
         }
     }
+
+    void darDano()
+    {
+        _areaDeDano.transform.localPosition = GetComponent<SpriteRenderer>().flipX == true 
+            ? new Vector2(-1.86f, 1) : new Vector2(0, 1);
+        Collider2D[] colider = new Collider2D[3];
+        _areaDeDano.GetComponent<BoxCollider2D>().OverlapCollider(new ContactFilter2D(), colider);
+        foreach (Collider2D colisao in colider)
+        {
+            if (colisao != null && colisao.gameObject.CompareTag("Player"))
+            {
+                _playerScript = colisao.GetComponent<Player>();
+                _playerScript.tomarDano(_dano, true);
+            }
+        }
+    }
+
+    IEnumerator tempodeAtaque()
+    {
+        yield return new WaitForSeconds(1.5f);
+        podeAtacar = true;
+        podeAndar = true;
+    }
+    //////////
+
+    //Segir//
+    void seguir()
+    {
+
+        if (Vector2.Distance(transform.position, _player.transform.position) < 10
+&& Vector2.Distance(transform.position, _player.transform.position) > 1.5f)
+        {
+            GetComponent<SpriteRenderer>().flipX = _player.transform.position.x < transform.position.x
+? true : false;
+            if (podeAndar)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, _player.transform.position, 4 * Time.deltaTime);
+                _anim.SetBool("andando", true);
+            }
+            if (Vector2.Distance(transform.position, _player.transform.position) <= 1.5f)
+            {
+                _anim.SetBool("ataque", false);
+                _anim.SetBool("andando", false);
+            }
+        }
+
+    }
+    /////////
+
+    //Vida//
+    private void Morrer()
+    {
+        Destroy(gameObject);
+    }
+
+    public void dano(int dano, bool darDano)
+    {
+        if (darDano)
+        {
+            _vida -= dano;
+            darDano = false;
+        }
+
+    }
+    ////////
 }
--- Project2/Assets/Scripts/Player.cs
+++ Project2/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -13,7 +14,8 @@ public class Player : MonoBehaviour
     float direcao = 1;
     [Header("Ataque")]
     bool podeAtacar = true;
-    public GameObject _area;    
+    public GameObject _area;
+    Inimigo inimigoScript;
 
     void Start()
     {
@@ -31,8 +33,27 @@ public class Player : MonoBehaviour
         {
             _anim.SetBool("caiu", false);
         }
+        if (_data._vida <= 0)
+            morrer();
     }
 
+    //Vida//
+    public void morrer()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void tomarDano(int dano, bool pode)
+    {
+        if (pode)
+        {
+            _data._vida -= dano;
+            pode = false;
+        }
+    }
+    ///////
+    
+    //Movimentacao//
     private void Pulo(bool pulou)
     {
         if (pulou && podePular)
@@ -45,43 +66,7 @@ public class Player : MonoBehaviour
     IEnumerator pular()
     {
         yield return new WaitForSeconds(0.15f);
-        _rb2D.AddForce(Vector2.up * 500);
-    }
-
-    private void Atacar(bool atacou)
-    {
-        _area.transform.localPosition = direcao < 0 ? new Vector2(-0.6f, -0.2f) : new Vector2(0,-0.2f);
-        if (atacou)
-            if (podeAtacar)
-            {
-                _anim.SetBool("ataque", true);
-                podeAtacar = false;
-                dano(GameObject.Find("arma"));   
-                StartCoroutine(ataque());
-            }
-            else
-                _anim.SetBool("ataque", false);
-    }
-
-    void dano(GameObject area)
-    {
-        Collider2D[] colider = new Collider2D[3];
-        area.GetComponent<BoxCollider2D>().OverlapCollider(new ContactFilter2D(), colider);
-        foreach (Collider2D colisao in colider)
-        {
-            if(colisao != null && colisao.gameObject.CompareTag("inimigo"))
-            {
-                colisao.GetComponent<Inimigo>()._vida -= _data._dano;
-            }
-        }
-    }
-
-    IEnumerator ataque()
-    {
-        yield return new WaitForSeconds(0.5f);
-        _anim.SetBool("ataque", false);
-        yield return new WaitForSeconds(_data._velocidadeDeAtaque - 0.5f);
-        podeAtacar = true;
+        _rb2D.AddForce(Vector2.up * 750);
     }
 
     private void Mover(bool mover)
@@ -105,6 +90,47 @@ public class Player : MonoBehaviour
             GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
         }
     }
+    ////////////////
+    
+    //Ataque//
+    private void Atacar(bool atacou)
+    {
+        _area.transform.localPosition = direcao < 0 ? new Vector2(-0.6f, -0.2f) : new Vector2(0,-0.2f);
+        if (atacou)
+            if (podeAtacar)
+            {
+                _anim.SetBool("ataque", true);
+                podeAtacar = false;
+                dano(GameObject.Find("arma"));   
+                StartCoroutine(ataque());
+            }
+            else
+                _anim.SetBool("ataque", false);
+    }
+
+    void dano(GameObject area)
+    {
+        Collider2D[] colider = new Collider2D[3];
+        area.GetComponent<BoxCollider2D>().OverlapCollider(new ContactFilter2D(), colider);
+        foreach (Collider2D colisao in colider)
+        {
+            if(colisao != null && colisao.gameObject.CompareTag("inimigo"))
+            {
+                inimigoScript = colisao.GetComponent<Inimigo>();
+                inimigoScript.dano(_data._dano, true);
+            }
+        }
+    }
+
+    IEnumerator ataque()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _anim.SetBool("ataque", false);
+        yield return new WaitForSeconds(_data._velocidadeDeAtaque - 0.5f);
+        podeAtacar = true;
+    }
+    /////////
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
